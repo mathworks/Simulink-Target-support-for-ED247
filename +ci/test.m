a54d3ed55de768b35bfeb8560f01a7119aab018a
Olivier Dufour <odufour@mathworks.com>
2021-04-01 14:42:11 +0200

    feat: Implements package function

--- +ci/test.m
+++ +ci/test.m
@@ -4,18 +4,20 @@ status  = -1;
 results = matlab.unittest.TestResult.empty;
 
 try
+       
+    rootfolder = regexprep(mfilename('fullpath'),'\+.*','');
             
-    proj = slproject.getCurrentProjects();
-    if isempty(proj)
-        rootfolder = regexprep(mfilename('fullpath'),'\+.*','');
-        proj = simulinkproject(rootfolder);
-    end
-    
     p = inputParser();
-    p.addParameter('Coverage',  fullfile(proj.RootFolder,sprintf('CoverageResults-r%s.xml',version('-release'))),@(x) validateattributes(x,{'string','char'},{'nonempty'}))
-    p.addParameter('Report',    fullfile(proj.RootFolder,sprintf('TestReport-r%s.pdf',version('-release'))),     @(x) validateattributes(x,{'string','char'},{'nonempty'}))
-    p.addParameter('TAPFile',   fullfile(proj.RootFolder,sprintf('TAPResults-r%s.tap',version('-release'))),     @(x) validateattributes(x,{'string','char'},{'nonempty'}))
+    p.addParameter('Mode',      'ci',   @(x) ismember(x,{'ci','dev','prod'}))
+    p.addParameter('Coverage',  fullfile(rootfolder,sprintf('CoverageResults-r%s.xml',version('-release'))),@(x) validateattributes(x,{'string','char'},{'nonempty'}))
+    p.addParameter('Report',    fullfile(rootfolder,sprintf('TestReport-r%s.pdf',version('-release'))),     @(x) validateattributes(x,{'string','char'},{'nonempty'}))
+    p.addParameter('TAPFile',   fullfile(rootfolder,sprintf('TAPResults-r%s.tap',version('-release'))),     @(x) validateattributes(x,{'string','char'},{'nonempty'}))
     parse(p,varargin{:})
+    
+    proj = slproject.getCurrentProjects();
+    if ismember(p.Results.Mode,{'ci','dev'}) && isempty(proj)
+        simulinkproject(rootfolder);
+    end
     
     ts = matlab.unittest.TestSuite.fromPackage('ed247', 'IncludeSubpackages', true);
     
@@ -24,7 +26,7 @@ try
     %
     % Coverage
     %
-    if ~verLessThan('MATLAB','9.3') % r2017b
+    if ismember(p.Results.Mode,{'ci'}) && ~verLessThan('MATLAB','9.3') % r2017b
         reportFile = p.Results.Coverage;
         reportFormat = matlab.unittest.plugins.codecoverage.CoberturaFormat(reportFile);
         plugin = matlab.unittest.plugins.CodeCoveragePlugin.forPackage('ed247','Producing',reportFormat);
@@ -34,20 +36,24 @@ try
     %
     % TAP
     %
-    tapFile = p.Results.TAPFile;
-    if exist(tapFile,'file') == 2
-        delete(tapFile)
+    if ismember(p.Results.Mode,{'ci'})
+        tapFile = p.Results.TAPFile;
+        if exist(tapFile,'file') == 2
+            delete(tapFile)
+        end
+        tapFile = matlab.unittest.plugins.ToFile(tapFile);
+        plugin  = matlab.unittest.plugins.TAPPlugin.producingVersion13(tapFile);
+        tr.addPlugin(plugin)
     end
-    tapFile = matlab.unittest.plugins.ToFile(tapFile);
-    plugin  = matlab.unittest.plugins.TAPPlugin.producingVersion13(tapFile);
-    tr.addPlugin(plugin)
     
     %
     % Report
     %
-    pdfFile = p.Results.Report;
-    plugin = matlab.unittest.plugins.TestReportPlugin.producingPDF(pdfFile);
-    tr.addPlugin(plugin)
+    if ismember(p.Results.Mode,{'ci'})
+        pdfFile = p.Results.Report;
+        plugin = matlab.unittest.plugins.TestReportPlugin.producingPDF(pdfFile);
+        tr.addPlugin(plugin)
+    end
     
     %
     % Run and get results
