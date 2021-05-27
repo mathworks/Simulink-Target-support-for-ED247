ed089bb39a1694098d95df4dcd7f5a5f0e586286
Olivier Dufour <odufour@mathworks.com>
2021-05-27 09:48:27 +0200

    tests: Add a test without refresh

--- tests/+ed247/+blocks/SendTest.m
+++ tests/+ed247/+blocks/SendTest.m
@@ -56,11 +56,68 @@ classdef (SharedTestFixtures={...
             warning('on')
                         
             % [ VERIFY ]
+            sendports = get(sendblock,'PortHandles');
+            testCase.verifyLength(sendports.Inport,4, ...
+                sprintf('Send block should have %d ports (refresh is disabled)', 4))
+            
             sendmask = ed247.blocks.Send(sendblock);
             actual = sendmask.PortLabel;
             Type = repmat({'input'},4,1);
             Number = (1:4)';
             Label = {'A429_BUS_2_MSG_1';'A429_BUS_2_MSG_1_refresh';'A429_BUS_2_MSG_2';'A429_BUS_2_MSG_2_refresh'};
+            expected = table(Type,Number,Label);            
+            testCase.verifyEqual(actual,expected, ...
+                'Send port labels does not match expected')
+            
+        end
+        
+        function testDisplayWithoutRefresh(testCase)
+            %
+            % Derivated from Issue018 test
+            %
+            
+            % [ SETUP ]
+            archivename = fullfile(testCase.filefolder_, 'A429_simple01.zip');
+            unzip(archivename,pwd)
+            
+            modelname = 'sendsimplea429';
+            new_system(modelname)
+            load_system(modelname)
+            closeModel = onCleanup(@() bdclose(modelname));
+            
+             %
+            % Add blocks in the model
+            %
+            configurationblockname = [modelname,'/Configure'];
+            configurationblock = add_block('lib_ed247/ED247_Configuration', configurationblockname);
+            
+            sendblockname = [modelname,'/Send'];
+            sendblock = add_block('lib_ed247/ED247_Send', sendblockname);
+            
+            %
+            % Configure blocks
+            %   - configuration file
+            %   - Enable refresh
+            %
+            set(configurationblock, 'configurationFilename', '''a429_01_ecic_in.xml''')
+            set(sendblock, 'enable_refresh', 'off')
+            
+            % [ EXERCISE ]
+            % Run SIM to update diagram only (do not care about warnings)
+            warning('off')
+            sim(modelname,'StopTime','0');
+            warning('on')
+                        
+            % [ VERIFY ]
+            sendports = get(sendblock,'PortHandles');
+            testCase.verifyLength(sendports.Inport,2, ...
+                sprintf('Send block should have %d ports (refresh is disabled)', 2))
+            
+            sendmask = ed247.blocks.Send(sendblock);
+            actual = sendmask.PortLabel;
+            Type = repmat({'input'},2,1);
+            Number = (1:2)';
+            Label = {'A429_BUS_2_MSG_1';'A429_BUS_2_MSG_2'};
             expected = table(Type,Number,Label);            
             testCase.verifyEqual(actual,expected, ...
                 'Send port labels does not match expected')
