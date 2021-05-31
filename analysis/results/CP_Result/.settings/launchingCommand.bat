09b71c6a4a524e0dda56dd65a9df20de7dd20015
Olivier Dufour <odufour@mathworks.com>
2021-05-31 11:13:20 +0200

    tests: Add comments and justifications in Polyspace results

--- analysis/results/CP_Result/.settings/launchingCommand.bat
+++ analysis/results/CP_Result/.settings/launchingCommand.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Files\PROGRAMS\Mathworks\Polyspace\R2020a\polyspace\bin\polyspace-code-prover.exe" -options-file "C:\Files\SANDBOX\Polyspace\Projects\ED247_for_Simulink\Module_1\CP_Result\.settings\options_command.txt" -results-dir "C:\Files\SANDBOX\Polyspace\Projects\ED247_for_Simulink\Module_1\CP_Result" %*
+"C:\Files\PROGRAMS\Mathworks\Polyspace\R2020a\polyspace\bin\polyspace-code-prover.exe" -options-file "C:\Files\SANDBOX\git\ED247\ed247_for_simulink\analysis\results\CP_Result\.settings\options_command.txt" -results-dir "C:\Files\SANDBOX\git\ED247\ed247_for_simulink\analysis\results\CP_Result" %*
