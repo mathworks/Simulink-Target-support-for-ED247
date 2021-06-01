e5264e409756346d2bd636eab25c6076934e9c3a
Olivier Dufour <odufour@mathworks.com>
2021-06-01 15:05:53 +0200

    tests: Clean test script (use variables)

--- tests/stubSFun.sh
+++ tests/stubSFun.sh
@@ -7,16 +7,15 @@
 #
 
 EXE=stubbedSFun.exe
-ARCHIVENAME=A429_simple01
-CONFIGURATIONFILE=a429_01_ecic_in
 
 # Inputs
-TEST_FILTER=${1:-""}
+ARCHIVENAME=${1:-"A429_simple01"}
+CONFIGURATIONFILE=${2:-"a429_01_ecic_in"}
 
 # Environment variables
 CC_OPTS=""
 if [ ! -z "$DEBUG" ]; then
-	CC_OPTS="${CC_OPTS} -DDEFINE_DEBUG=ON"
+	CC_OPTS="${CC_OPTS} -DDEBUG"
 fi
 
 # Tools
@@ -44,7 +43,13 @@ echo "## Delete previous executable"
 rm -f ${EXE}
 
 echo "## Compile the debug program for test purpose"
-gcc -o ${EXE} -DDEBUG -I${ROOT_FOLDER}/c_sources/include -I${ROOT_FOLDER}/libraries/ed247 -I${ROOT_FOLDER}/libraries/ed247/sources -I${ROOT_FOLDER}/work/stubbed_sfun -I${ROOT_FOLDER}/../ED247_LIBRARY/_install/inc -I${ROOT_FOLDER}/../libxml2/include -L${ROOT_FOLDER}/../ED247_LIBRARY/_install/lib ${ROOT_FOLDER}/libraries/ed247/ed247_sfun.c ${ROOT_FOLDER}/libraries/ed247/sources/ed247_sfun_configure.c ${ROOT_FOLDER}/libraries/ed247/sources/ed247_sfun_send.c ${ROOT_FOLDER}/libraries/ed247/sources/ed247_sfun_receive.c ${ROOT_FOLDER}/c_sources/src/ed247_cmd_xml.c ${ROOT_FOLDER}/c_sources/src/ed247_interface.c ${ROOT_FOLDER}/c_sources/src/tools.c ${ROOT_FOLDER}/work/stubbed_sfun/main.c -L${ROOT_FOLDER}/../libxml2/lib -lxml2 ${MINGW_FOLDER}/../x86_64-w64-mingw32/lib/libws2_32.a -led247
+INCLUDES="-I${ROOT_FOLDER}/c_sources/include -I${ROOT_FOLDER}/libraries/ed247 -I${ROOT_FOLDER}/libraries/ed247/sources -I${ROOT_FOLDER}/work/stubbed_sfun -I${ROOT_FOLDER}/../ED247_LIBRARY/_install/inc -I${ROOT_FOLDER}/../libxml2/include"
+INTERFACE_SOURCES="${ROOT_FOLDER}/c_sources/src/ed247_cmd_xml.c ${ROOT_FOLDER}/c_sources/src/ed247_interface.c ${ROOT_FOLDER}/c_sources/src/tools.c"
+LIBRARIES="-L${ROOT_FOLDER}/../libxml2/lib -lxml2 ${MINGW_FOLDER}/../x86_64-w64-mingw32/lib/libws2_32.a -L${ROOT_FOLDER}/../ED247_LIBRARY/_install/lib -led247"
+SFUN_SOURCES="${ROOT_FOLDER}/libraries/ed247/ed247_sfun.c ${ROOT_FOLDER}/libraries/ed247/sources/ed247_sfun_configure.c ${ROOT_FOLDER}/libraries/ed247/sources/ed247_sfun_send.c ${ROOT_FOLDER}/libraries/ed247/sources/ed247_sfun_receive.c"
+TEST_SOURCES="${ROOT_FOLDER}/work/stubbed_sfun/main.c"
+
+gcc -o ${EXE} ${CC_OPTS} ${INCLUDES} ${SFUN_SOURCES} ${INTERFACE_SOURCES} ${TEST_SOURCES} ${LIBRARIES}
 
 echo ""
 echo "## Prepare configuration files"
