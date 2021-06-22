6b369f1a137f258259722606fcc69362a03bcb21
Olivier Dufour <odufour@mathworks.com>
2021-06-22 11:20:48 +0200

    tests: Write test on createBus for Receive with full ELAC configuration

--- tests/+issues/Issue023.m
+++ tests/+issues/Issue023.m
@@ -43,7 +43,7 @@ classdef (SharedTestFixtures={...
             %
             % Configure blocks
             %   - configuration file
-            %   - Enable refresh
+            %   - Disable refresh
             %
             set(configurationblock, 'configurationFilename', '''a429_01_ecic_in.xml''')
             set(receiveblock, 'enable_refresh', 'off')
@@ -144,6 +144,149 @@ classdef (SharedTestFixtures={...
             content = inputs(:,2);
             content = [content{:}];
             expected = repmat({'data';'refresh'},1,3);
+            testCase.verifyEqual(content,expected, 'Each signal should contain data and refresh fields')
+            
+        end
+        
+        function testFullELACReceiveWithoutRefresh(testCase)
+            
+            % [ SETUP ]
+            % Load expected signals table
+            expecteddatafile = fullfile(testCase.filefolder_, 'ELACe2C_ECIC.mat');
+            expecteddata = load(expecteddatafile);
+            expecteddata = expecteddata.Receive;
+            
+            archivename = fullfile(testCase.filefolder_, 'ELAC_full.zip');
+            unzip(archivename,pwd)
+            
+            modelname = 'readfullelac';
+            new_system(modelname)
+            load_system(modelname)
+            closeModel = onCleanup(@() bdclose(modelname));
+            
+            %
+            % Add blocks in the model
+            %
+            configurationblockname = [modelname,'/Configure'];
+            configurationblock = add_block('lib_ed247/ED247_Configuration', configurationblockname);
+            
+            receiveblockname = [modelname,'/Receive'];
+            receiveblock = add_block('lib_ed247/ED247_Receive', receiveblockname);
+                        
+            %
+            % Configure blocks
+            %   - configuration file
+            %   - Disable refresh
+            %
+            set(configurationblock, 'configurationFilename', '''ELACe2C_ECIC.xml''')
+            set(receiveblock, 'enable_refresh', 'off')
+            
+            % Run SIM to update diagram only (do not care about warnings)
+            warning('off')
+            sim(modelname,'StopTime','0');
+            warning('on')
+            
+            receiveobj = ed247.blocks.Receive(receiveblock);
+            receiveobj.fWaitbar = @(varargin) MockWaitbar.waitbar(varargin{:});
+            
+            % [ EXERCISE ]
+            createBus(receiveobj)
+            
+            % [ VERIFY ]
+            receivesubsysblock = find_system(modelname,'FindAll','on','SearchDepth',1,'Type','block','Name','Receive');
+            testCase.assertLength(receivesubsysblock, 1, ...
+                sprintf('Model should contain 1 block named %s', 'Receive'))
+            testCase.assertEqual(get(receivesubsysblock,'BlockType'), 'SubSystem', ...
+                sprintf('Block %s should be a subsystem', 'Receive'))
+            
+            ports = get(receivesubsysblock,'PortHandles');
+            testCase.verifyEmpty(ports.Inport, 'Receive subsystem should not have any inport')
+            testCase.verifyLength(ports.Outport, 1, 'Receive subsystem should have 1 outport')
+            
+            busselector = add_block('simulink/Signal Routing/Bus Selector',[modelname,'/bs']);
+            pos = get(receivesubsysblock,'Position');
+            set(busselector, 'Position', pos + [100,0,100,0])
+            add_line(modelname,'Receive/1','bs/1')
+            
+            inputs = get(busselector,'InputSignals');
+            expected = convertStringsToChars(expecteddata.SignalName);
+            testCase.verifyEqual(inputs,expected)
+            
+        end
+        
+        function testFullELACReceiveWithRefresh(testCase)
+            
+            % [ SETUP ]
+            % Load expected signals table
+            expecteddatafile = fullfile(testCase.filefolder_, 'ELACe2C_ECIC.mat');
+            expecteddata = load(expecteddatafile);
+            expecteddata = expecteddata.Receive;
+            
+            archivename = fullfile(testCase.filefolder_, 'ELAC_full.zip');
+            unzip(archivename,pwd)
+            
+            modelname = 'readfullelac';
+            new_system(modelname)
+            load_system(modelname)
+            closeModel = onCleanup(@() bdclose(modelname));
+            
+            %
+            % Add blocks in the model
+            %
+            configurationblockname = [modelname,'/Configure'];
+            configurationblock = add_block('lib_ed247/ED247_Configuration', configurationblockname);
+            
+            receiveblockname = [modelname,'/Receive'];
+            receiveblock = add_block('lib_ed247/ED247_Receive', receiveblockname);
+                        
+            %
+            % Configure blocks
+            %   - Configuration file
+            %   - Enable refresh
+            %
+            set(configurationblock, 'configurationFilename', '''ELACe2C_ECIC.xml''')
+            set(receiveblock, 'enable_refresh', 'on')
+            
+            % Run SIM to update diagram only (do not care about warnings)
+            warning('off')
+            sim(modelname,'StopTime','0');
+            warning('on')
+            
+            receiveobj = ed247.blocks.Receive(receiveblock);
+            receiveobj.fWaitbar = @(varargin) MockWaitbar.waitbar(varargin{:});
+            
+            % [ EXERCISE ]
+            createBus(receiveobj)
+            
+            % [ VERIFY ]
+            receivesubsysblock = find_system(modelname,'FindAll','on','SearchDepth',1,'Type','block','Name','Receive');
+            testCase.assertLength(receivesubsysblock, 1, ...
+                sprintf('Model should contain 1 block named %s', 'Receive'))
+            testCase.assertEqual(get(receivesubsysblock,'BlockType'), 'SubSystem', ...
+                sprintf('Block %s should be a subsystem', 'Receive'))
+            
+            ports = get(receivesubsysblock,'PortHandles');
+            testCase.verifyEmpty(ports.Inport, 'Receive subsystem should not have any inport')
+            testCase.verifyLength(ports.Outport, 1, 'Receive subsystem should have 1 outport')
+            
+            busselector = add_block('simulink/Signal Routing/Bus Selector',[modelname,'/bs']);
+            pos = get(receivesubsysblock,'Position');
+            set(busselector, 'Position', pos + [100,0,100,0])
+            add_line(modelname,'Receive/1','bs/1')
+            
+            inputs = get(busselector,'InputSignals');            
+            mask = cellfun(@length,inputs) == 2;
+            tmp = vertcat(inputs{mask});            
+            signames = cell(size(inputs));
+            signames(~mask) = inputs(~mask);
+            signames(mask) = tmp(:,1);
+            
+            expected = convertStringsToChars(expecteddata.SignalName);
+            testCase.verifyEqual(signames,expected)
+            
+            content = tmp(:,2);
+            content = [content{:}];
+            expected = repmat({'data';'refresh'},1,461);
             testCase.verifyEqual(content,expected, 'Each signal should contain data and refresh fields')
             
         end
