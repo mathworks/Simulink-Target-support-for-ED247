afbc347881cf6739aee85cd4187a65ff4509f57d
promero <promero@mathworks.com>
2021-10-25 11:10:56 +0200

    Fixed paths for new ED247 core library and MinGW clean up.

--- sources/+ed247/compile.m
+++ sources/+ed247/compile.m
@@ -19,17 +19,12 @@ c = onCleanup(@() cd(currentdir));
 config = ed247.Configuration.default();
 
 %% Parameters
-mingwpath       = config.MinGW;
 mexfolder       = config.MEX;
 adapterfolder   = config.Adapter;
 ed247folder     = config.ED247;
 libxml2folder   = config.LibXML2;
 
 sfunsourcefolder = ed247.Folder.SFUN_SOURCES.Path;
-
-if ispc()
-    assert(isdir(mingwpath), sprintf('Invalid MinGW configuration "%s".\nSet folder using <a href="matlab:ed247.ConfigurationUI.open()">Configuration App</a>.', mingwpath)) %#ok<ISDIR>
-end
 
 assert(isdir(ed247folder), sprintf('Invalid ED247 configuration "%s".\nSet folder using <a href="matlab:ed247.ConfigurationUI.open()">Configuration App</a>.', ed247folder)) %#ok<ISDIR>
 assert(isdir(libxml2folder), sprintf('Invalid LibXML2 configuration "%s".\nSet folder using <a href="matlab:ed247.ConfigurationUI.open()">Configuration App</a>.', libxml2folder)) %#ok<ISDIR>
@@ -53,34 +48,21 @@ defines             = {};
 opts                = {};
 
 % Source files
-sourcefiles{end+1} = fullfile(adapterfolder, 'src',  'ed247_cmd_xml.cpp');
-sourcefiles{end+1} = fullfile(adapterfolder, 'src',  'ed247_interface.cpp');
-sourcefiles{end+1} = fullfile(adapterfolder, 'src',  'tools.cpp');
+sourcefiles{end+1} = fullfile(adapterfolder, 'src', 'ed247_cmd_xml.cpp');
+sourcefiles{end+1} = fullfile(adapterfolder, 'src', 'ed247_interface.cpp');
+sourcefiles{end+1} = fullfile(adapterfolder, 'src', 'tools.cpp');
 
 if isunix()
-    sourcefiles{end+1} = fullfile(ed247folder,   'lib',  'libed247.so');
-    sourcefiles{end+1} = fullfile(libxml2folder, 'lib',  'libxml2.a');
+    sourcefiles{end+1} = fullfile(ed247folder, 'lib', 'libed247_static.a');
+elseif ispc()
+    sourcefiles{end+1} = fullfile(ed247folder, 'lib', 'ed247.lib');
 else
-    
-    sourcefiles{end+1} = fullfile(ed247folder,   'lib',  'libed247.dll.a');
-    
-    dllfile = fullfile(libxml2folder, 'lib',  'libxml2.dll.a');
-    afile   = fullfile(libxml2folder, 'lib',  'libxml2.a');
-    if exist(dllfile,'file') == 2
-        sourcefiles{end+1} = dllfile;
-    else
-        sourcefiles{end+1} = afile;
-    end
-    
-    % FIXME : Ws2_32 library is usually included with -l flag
-    sourcefiles{end+1} = fullfile(mingwpath,'x86_64-w64-mingw32','lib','libws2_32.a');
-    % End FIXME
-    
+    error('Not supported OS.');
 end
 
 % Include directories
 includedirectories{end+1} = fullfile(adapterfolder, 'include');
-includedirectories{end+1} = fullfile(ed247folder,   'inc');
+includedirectories{end+1} = fullfile(ed247folder,   'include');
 includedirectories{end+1} = fullfile(libxml2folder, 'include','libxml2');
 includedirectories{end+1} = fullfile(libxml2folder, 'include');
 includedirectories{end+1} = sfunsourcefolder;
@@ -128,7 +110,6 @@ for i_mex = 1:numel(mexfiles)
         sourcefiles{end+1} = fullfile(sfunsourcefolder, 'ed247_sfun_send.cpp'); %#ok<AGROW>
     end
             
-    warning('off','MATLAB:mex:MinGWVersion_link')
     if p.Results.EnablePolyspace
         opts{end+1} = '-sldv'; %#ok<AGROW>
         fprintf(1, 'slcovmex %s %s %s %s %s \n', strjoin(opts,' '), '-outdir', p.Results.OutputFolder, cmexsfcn, strjoin([sourcefiles, includedirectories, defines],' '));
@@ -137,6 +118,5 @@ for i_mex = 1:numel(mexfiles)
         fprintf(1, 'mex %s %s %s %s %s \n', strjoin(opts,' '), '-outdir', p.Results.OutputFolder, cmexsfcn, strjoin([sourcefiles, includedirectories, defines],' '));
         mex(opts{:},'-outdir',p.Results.OutputFolder,cmexsfcn,sourcefiles{:}, includedirectories{:},defines{:});
     end
-    warning('on','MATLAB:mex:MinGWVersion_link')
     
 end
